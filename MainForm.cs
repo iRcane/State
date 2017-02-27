7935739d21b177a1c686d9b1599d7e6a1519e8de
QPCA <qpca@outlook.com>
2017-02-28 01:16:02 +0700

    lol

---
+++ MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace State
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public IState State { get; set; }
+
+        public bool isFileNameEmpty()
+        {
+            if (fileName.Text.Trim(' ', '\t') == "")
+                return true;
+            else
+                return false;
+        }
+
+        public bool isFileContentEmpty()
+        {
+            if (fileContent.Text == "")
+                return true;
+            else
+                return false;
+        }
+
+        public string GetFileName()
+        {
+            return fileName.Text;
+        }
+
+        public string GetFileContent()
+        {
+            return fileContent.Text;
+        }
+
+        private void fileName_TextChanged(object sender, EventArgs e)
+        {
+            State = State.SwitchState(this);
+        }
+
+        private void fileContent_TextChanged(object sender, EventArgs e)
+        {
+            State = State.SwitchState(this);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            State.CloseHandler(sender, e);
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            State.Save(this.GetFileName(), this.GetFileContent());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace State
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new MainForm();
+            InitialState state = new InitialState();
+            form.State = state;
+            
+            Application.Run(form);
+        }
+    }
+
+    public interface IState
+    {
+        IState SwitchState(MainForm f);
+        void CloseHandler(object sender, FormClosingEventArgs e);
+        void Save(string fileName, string fileContent);
+    }
+
+    class InitialState: IState
+    {
+        public IState SwitchState(MainForm f)
+        {
+            if (f.isFileNameEmpty())
+                return new NoNameState();
+            else if (f.isFileContentEmpty())
+                return new EmptyState();
+            return new ChangedState();
+        }
+
+        public void CloseHandler(object sender, FormClosingEventArgs e)
+        {
+            var form = (MainForm)sender;
+            form.Close();
+        }
+
+        public void Save(string fileName, string fileContent)
+        {
+            MessageBox.Show("Can not save changes as no file name entered.", "Warning", MessageBoxButtons.OK);
+        }
+    }
+
+    class EmptyState: IState
+    {
+        public IState SwitchState(MainForm f)
+        {
+            if (f.isFileNameEmpty())
+                return new NoNameState();
+            else if (f.isFileContentEmpty())
+                return this;
+            return new ChangedState();
+        }
+
+        public void CloseHandler(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Changes might not been saved. Save changes?", "Warning", MessageBoxButtons.YesNoCancel);
+            var form = (MainForm)sender;
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    Save(form.GetFileName(), form.GetFileContent());
+                    break;
+                case DialogResult.No:
+                    break;
+                default:
+                    e.Cancel = true;
+                    break;
+            }
+        }
+
+        public void Save(string fileName, string fileContent)
+        {
+            var result = MessageBox.Show("Are you sure you want to save an empty file?", "Warning", MessageBoxButtons.YesNo);
+
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    System.IO.File.WriteAllText(fileName, fileContent);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    class ChangedState: IState
+    {
+        public IState SwitchState(MainForm f)
+        {
+            if (f.isFileNameEmpty())
+                return new NoNameState();
+            else if (f.isFileContentEmpty())
+                return new EmptyState();
+            else
+                return this;
+        }
+
+        public void CloseHandler(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Changes might not been saved. Save changes?", "Warning", MessageBoxButtons.YesNoCancel);
+            var form = (MainForm)sender;
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    Save(form.GetFileName(), form.GetFileContent());
+                    break;
+                case DialogResult.No:
+                    break;
+                default:
+                    e.Cancel = true;
+                    break;
+            }
+        }
+
+        public void Save(string fileName, string fileContent)
+        {
+            System.IO.File.WriteAllText(fileName, fileContent);
+        }
+    }
+
+    class NoNameState: IState
+    {
+        public IState SwitchState(MainForm f)
+        {
+            if (!f.isFileNameEmpty())
+                return new ChangedState();
+            else
+                return this;
+        }
+
+        public void CloseHandler(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Closing will not save changes as no file name entered. Continue closing?", "Warning", MessageBoxButtons.YesNo);
+            var form = (MainForm)sender;
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    break;
+                default:
+                    e.Cancel = true;
+                    break;
+            }
+        }
+
+        public void Save(string fileName, string fileContent)
+        {
+            MessageBox.Show("Can not save changes as no file name entered.", "Warning", MessageBoxButtons.OK);
+        }
+    }
+}
