75f6dbcc4237ea5fda48ed7eaca777b0e8535be0
QPCA <qpca@outlook.com>
2017-02-28 17:07:34 +0700

    Proj

---
+++ State/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace State
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.fileContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(15, 214);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 0;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.Location = new System.Drawing.Point(15, 35);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(257, 20);
+            this.fileName.TabIndex = 1;
+            this.fileName.TextChanged += new System.EventHandler(this.fileName_TextChanged);
+            // 
+            // fileContent
+            // 
+            this.fileContent.Location = new System.Drawing.Point(15, 89);
+            this.fileContent.Multiline = true;
+            this.fileContent.Name = "fileContent";
+            this.fileContent.Size = new System.Drawing.Size(257, 119);
+            this.fileContent.TabIndex = 2;
+            this.fileContent.TextChanged += new System.EventHandler(this.fileContent_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path to file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Content";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileContent);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.SaveBtn);
+            this.Name = "MainForm";
+            this.Text = "State";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.TextBox fileContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
